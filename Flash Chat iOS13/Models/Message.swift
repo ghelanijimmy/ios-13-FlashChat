1165561ebf158a536f49cc0a4fd543a4b9ae5f33
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-18 09:46:42 -0400

    add firebase auth and dab and view for messages in table view and load messages on load and send message

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Jimmy Ghelani on 2020-07-17.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct Message {
+    let sender: String
+    let body: String
+}
